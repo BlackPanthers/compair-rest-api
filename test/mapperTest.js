870d359e1172d1202fa4d60f01dcfa6315315365
William Kwao <williamkwao@Williams-MacBook-Pro-2.local>
2018-01-13 10:22:20 -0500

    updated mapper tests

--- test/mapperTest.js
+++ test/mapperTest.js
@@ -1,10 +1,13 @@
 var assert = require('assert');
+var _ = require('lodash');
 const mockAmzResponse = require('./mocks/mockAmzResponse');
 const mockWalmartResponse = require('./mocks/mockWalmartResponse');
 const helperFunctions = require('../helperFunctions');
 
 helperFunctions.mapAmzToCompair(mockAmzResponse);
 helperFunctions.mapWalmartToCompair(mockWalmartResponse);
+
+const expectedKeys = ['retailer', 'name', 'shortDescription', 'salePrice', 'mediumImage', 'productURL'];
 
 describe('Amazon Mapper Test', function () {
     it('Should return the same number of items in mock element', function () {
@@ -17,14 +20,47 @@ describe('Amazon Mapper Test', function () {
         var amazonItems = mockAmzResponse.ItemSearchResponse.Items.Item;
         assert.equal(amazonItems[0].ItemAttributes.Title, mappedResponse[0].name);
     });
+
+    it('Should return empty array yon empty item response', function () {
+        const emptyResponse = {
+            ItemSearchResponse: {
+                Items: {
+                    item: []
+                }
+            }
+        }
+        var mappedResponse = helperFunctions.mapAmzToCompair(emptyResponse);
+        assert(_.isEmpty(mappedResponse))
+    });
+});
+
+describe('Walmart Mapper Test', function () {
+    it('Should return the same number of items in mock element', function () {
+        var mappedResponse = helperFunctions.mapWalmartToCompair(mockWalmartResponse);
+        assert.equal(mappedResponse.length, mockWalmartResponse.items.length);
+    });
+
+    it('Should return the same needed elements', function () {
+        var mappedResponse = helperFunctions.mapWalmartToCompair(mockWalmartResponse);
+        var walmartItems = mockWalmartResponse.items;
+        assert.equal(walmartItems[0].name, mappedResponse[0].name);
+    });
+
+    it('Should return empty array yon empty item response', function () {
+        const emptyResponse = {
+            items: []
+        }
+        var mappedResponse = helperFunctions.mapWalmartToCompair(emptyResponse);
+        assert(_.isEmpty(mappedResponse))
+    });
 });
 
 describe('Relevance sort test', function () {
     it('Should return the sum of two arrays', function () {
         var testamz = helperFunctions.mapAmzToCompair(mockAmzResponse);
         var testwm = helperFunctions.mapWalmartToCompair(mockWalmartResponse);
-       var result = helperFunctions.relevanceSort(testamz,testwm);
-       assert.equal(testamz.length + testwm.length , result.length);
+        var result = helperFunctions.relevanceSort(testamz, testwm);
+        assert.equal(testamz.length + testwm.length, result.length);
     });
 
     it('Should return the sum of two arrays with different length', function () {
@@ -33,11 +69,11 @@ describe('Relevance sort test', function () {
         testamz.pop();
         console.log("Length of amazon after pop - " + testamz.length)
         var testwm = helperFunctions.mapWalmartToCompair(mockWalmartResponse);
-        console.log("Length of Walmart -  " +testwm.length)
-       var result = helperFunctions.relevanceSort(testamz,testwm);
-       assert.equal(testamz.length + testwm.length , result.length);
+        console.log("Length of Walmart -  " + testwm.length)
+        var result = helperFunctions.relevanceSort(testamz, testwm);
+        assert.equal(testamz.length + testwm.length, result.length);
     });
-    
+
 });
 
 
