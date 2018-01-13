3cf47bd74a2892a3387814b8e689ce18f75fc35c
William Kwao <williamkwao@Williams-MacBook-Pro-2.local>
2018-01-12 21:35:07 -0500

    updated promise chains

--- server.js
+++ server.js
@@ -8,34 +8,58 @@ const opHelper = new OperationHelper(apiKeys.amazondetails);
 
 
 app.get('/search/:keyword', (req, res) => {
+    const keyword = req.params.keyword;
+    var mappedAmzResponse = [];
+    var mappedWalmartResponse = [];
 
-    opHelper.execute('ItemSearch', {
-        'SearchIndex': 'All',
-        'Keywords': req.params.keyword,
-        'ResponseGroup': 'ItemAttributes,Offers,Images'
-    }).then((response) => {
-        console.log("Results object: ", response.result);
-        var mappedAmzResponse = helperFunctions.mapAmzToCompair(response.result);
-        walmart.search(req.params.keyword, "").then((items) => {
-            var mappedWalmartResponse = helperFunctions.mapWalmartToCompair(items);
+    //make amazon request
+    searchAmazon(keyword)
+        .then((amazonResponse) => {
+            console.log("Results object for amazon: ", amazonResponse.result);
+            mappedAmzResponse = helperFunctions.mapAmzToCompair(amazonResponse.result);
+            return walmart.search(keyword, "");
+        }).then((walmartResponse) => {
+            mappedWalmartResponse = helperFunctions.mapWalmartToCompair(walmartResponse);
             var mappedRes = mappedWalmartResponse.concat(mappedAmzResponse);
             var sortedMappedRes = mappedRes.sort((a, b) => {
                 return parseFloat(a.salePrice) - parseFloat(b.salePrice);
             })
-
-            console.log(sortedMappedRes);
             var responseObject = {
                 items: sortedMappedRes,
             }
             res.send(responseObject);
-        }).catch((err) => {
-            console.log(err);
+        })
+        .catch((err) => {
+            console.error("Something went wrong! ", err);
+        });
+});
+
+const searchAmazon = (keyword) => {
+    return new Promise((resolve, reject) => {
+        opHelper.execute('ItemSearch', {
+            'SearchIndex': 'All',
+            'Keywords': keyword,
+            'ResponseGroup': 'ItemAttributes,Offers,Images'
+        }).then((response) => {
+            resolve(response);
+        }).catch((error) => {
+            reject(error);
         });
 
-    }).catch((err) => {
-        console.error("Something went wrong! ", err);
     });
-});
+}
+
+app.get('/amazon/:keyword', (req, res) => {
+    var amazonResponseStore = {}
+    var walmartResponseStore = {}
+    searchAmazon(req.params.keyword)
+        .then((amazonResponse) => {
+            amazonResponseStore = amazonResponse;
+            res.send(amazonResponseStore.result);
+        })
+        .catch();
+})
+
 
 
 app.listen(8080, () => console.log('COMPAIR REST API listening on port 8080!'))
