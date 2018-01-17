b4a1521f2a5c684c35da3c649b73a28f47b982a4
William Kwao <williamkwao@Williams-MacBook-Pro-2.local>
2018-01-17 01:27:16 -0500

    fix for items with no price

--- helperFunctions.js
+++ helperFunctions.js
@@ -9,37 +9,43 @@ const mapAmzToCompair = (amazonResponse) => {
         console.log("Stated mapping amazon item ASIN: " + item.ASIN)
 
         //if item is not available return price of 0
+
         var price = 0;
-        if (item.ItemAttributes.ListPrice || item.OfferSummary.LowestNewPrice) {
-            price = (item.ItemAttributes.ListPrice == undefined) ? item.OfferSummary.LowestNewPrice.Amount : item.ItemAttributes.ListPrice.Amount;
-        }
-        var smallImage, mediumImage, largeImage;
+        try {
+            if (item.ItemAttributes.ListPrice || item.OfferSummary.LowestNewPrice) {
+                price = (item.ItemAttributes.ListPrice == undefined) ? item.OfferSummary.LowestNewPrice.Amount : item.ItemAttributes.ListPrice.Amount;
+            }
+            var smallImage, mediumImage, largeImage;
 
-        //if some form of image exists
-        if (item.ImageSet || item.MediumImage) {
-            var ImageSet = item.ImageSets.ImageSet;
-            var firstImageSet = (ImageSet.constructor === Array) ? ImageSet[0] : ImageSet;
-            smallImage = (item.SmallImage == undefined) ? firstImageSet.SmallImage.URL : item.SmallImage.URL;
-            mediumImage = ("MediumImage" in item) ? item.MediumImage.URL : firstImageSet.MediumImage.URL;
-            largeImage = (item.LargeImage == undefined) ? firstImageSet.LargeImage.URL : item.LargeImage.URL;
-        }
+            //if some form of image exists
+            if (item.ImageSet || item.MediumImage) {
+                var ImageSet = item.ImageSets.ImageSet;
+                var firstImageSet = (ImageSet.constructor === Array) ? ImageSet[0] : ImageSet;
+                smallImage = (item.SmallImage == undefined) ? firstImageSet.SmallImage.URL : item.SmallImage.URL;
+                mediumImage = ("MediumImage" in item) ? item.MediumImage.URL : firstImageSet.MediumImage.URL;
+                largeImage = (item.LargeImage == undefined) ? firstImageSet.LargeImage.URL : item.LargeImage.URL;
+            }
 
-        var newItem = {
-            retailer: 'Amazon',
-            ASIN: item.ASIN,
-            name: item.ItemAttributes.Title,
-            shortDescription: item.ItemAttributes.Feature,
-            salePrice: (price * 0.01).toFixed(2),
-            offerSummary: item.OfferSummary,
-            offers: item.Offers,
-            mediumImage: mediumImage,
-            smallImage: smallImage,
-            largeImage: largeImage,
-            productURL: item.DetailPageURL,
-            images: item.ImageSets,
-        };
-        console.log("Sucessfully mapped amazon item: " + item.ASIN)
-        ret.push(newItem);
+            var newItem = {
+                retailer: 'Amazon',
+                ASIN: item.ASIN,
+                name: item.ItemAttributes.Title,
+                shortDescription: item.ItemAttributes.Feature,
+                salePrice: (price * 0.01).toFixed(2),
+                offerSummary: item.OfferSummary,
+                offers: item.Offers,
+                mediumImage: mediumImage,
+                smallImage: smallImage,
+                largeImage: largeImage,
+                productURL: item.DetailPageURL,
+                images: item.ImageSets,
+            };
+            console.log("Sucessfully mapped amazon item: " + item.ASIN);
+            ret.push(newItem);
+
+        } catch (error) {
+            console.error("Failed to map amazon item: " + item.ASIN + " "+error);
+        }
 
     }
     return ret;
